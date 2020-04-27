23578aa6be2713914a0400789de7724cdbeeb043
Annachiara Traverso <annachiara.traverso@gmail.com>
2020-04-27 20:53:20 +0200

    working exercise + bonus (no checkpoints)

--- take2.js
+++ take2.js
@@ -1,34 +1,77 @@
+/*Il computer deve generare 16 numeri casuali tra 1 e 100, che saranno le "mine".
+In seguito deve chiedere all'utente di inserire un numero alla volta, sempre compreso tra 1 e 100, che sarà la sua giocata.
+Se il numero è presente nella lista delle mine, la partita termina, altrimenti il gioco continua chiedendo all'utente un altro numero (continua a giocare).
+La partita termina quando il giocatore becca una mina, ossia inserisce un numero "vietato", oppure se raggiunge il numero massimo possibile di numeri consentiti, ossia ha inserito tutti i numeri possibili che non sono mine!
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha inserito un numero consentito; in altre parole, deve comunicare all'utente quante giocate ha fatto prima di perdere.
+BONUS: all'inizio della partita, il software richiede anche un livello di difficoltà all'utente che cambia il range di numeri totali (le mine saranno sempre 16):
+con difficoltà 0=> si gioca con numeri che vanno da 1 a 100
+con difficoltà 1 => si gioca con numeri che vanno da 1 a 80
+con difficoltà 2=> si gioca con numeri che vanno da 1 a 50
+*/
+
+/*ask for difficulty level*/
+var getDifficulty = prompt('Choose your level: 0 - easy, 1 - normal, 2 - difficult');
+
+if (getDifficulty == 0) {
+  level = 100 +1;
+} else if (getDifficulty == 1) {
+  level = 80 +1;
+} else if (getDifficulty == 3) {
+  level = 20 +1;
+} else {
+  level = 50 +1;
+}
+
 /*container array for random "losing" numbers*/
 var mines = [];
+
+/*populate with random "losing" numbers*/
+mines.push(getUniqueRandomNumbers(16, level));
+
 /*container array for max tries count*/
 var maxTriesCheck = [];
 
-/*filled in to check max tries, REMEMBER TO SUB W ACTUAL CODE*/
-for (var i = 1; i <= 20; i++) {
-  mines.push(i);
-}
-for (var i = 1; i <= 96; i++) {
-  maxTriesCheck.push(i);
-}
-console.log(mines);
-console.log(maxTriesCheck);
+/*copy losing numbers into total number count*/
+var maxTriesCheck = mines.slice();
+// console.log(mines);
+// console.log(maxTriesCheck);
 
-/*set a counter*/
+/*set a counter variable for scorekeeping*/
 var scoreKeeper = 1;
 
-/*ask for a number between 1 and 100*/
-
 do {
-  getUserNumber = parseInt(prompt('Pick a number between 1 and 100'));
+  /*ask for a number between 1 and what's required by the level of difficulty*/
+  getUserNumber = parseInt(prompt('Pick a number between 1 and ' + level));
+
+  /*if the number matches a mine: you lose, otherwise, push the number into total number count array*/
   if (mines.includes(getUserNumber)) {
     console.log('Your score is: ' + scoreKeeper);
   } else if (!maxTriesCheck.includes(getUserNumber)) {
     maxTriesCheck.push(getUserNumber);
   }
+  /*update score*/
   scoreKeeper++;
-  console.log(maxTriesCheck);
-} while (!mines.includes(getUserNumber) && maxTriesCheck.length < 100);
+  // console.log(maxTriesCheck);
+} /*keep running until total number conut reaches what's required by level of difficulty*/
+  while (!mines.includes(getUserNumber) && maxTriesCheck.length < level);
 
-if (maxTriesCheck.length >= 100) {
+/*if you manage to survive the do-while cycle then congrats, you're a winner!*/
+if (maxTriesCheck.length >= level) {
   console.log('You win');
+}
+
+/*FUNCTIONS - alphabetical order*/
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function getUniqueRandomNumbers(howMany, max) {
+  while (mines.length < howMany) {
+    var randomMineNumber = getRandomNumber(1, max);
+    if (!mines.includes(randomMineNumber)) {
+    mines.push(randomMineNumber);
+    }
+  }
+  return mines;
 }
